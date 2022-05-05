644d59b79ba370c49f86d561ac51e6b33af36d43
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-05 15:26:01 -0400

    load about in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import About from './componets/about';
 
 function App() {
   return (
     <div>
-
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
